23a1eea058839de81ec2541704f5f632e7132c69
Joshua Crotts <joshuacrotts@yahoo.com>
2018-08-02 00:47:11 -0400

    Added new line for push test 2

--- HelloWorld/HelloWorld/HelloWorldTwo.cpp
+++ HelloWorld/HelloWorld/HelloWorldTwo.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main(){
 
 	std::cout<<"I am another C++ file! Let's see if you can view it! \n";
+
+	//Comment
+	std::cout << "I am a line added by Joshua at 12:46 AM to test if pushing this works!\n";
 	
 	return 0;
 }
